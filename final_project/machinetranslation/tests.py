5a0ce4cb011ab6f3c99258c61989de4a76f40fad
IslamelGebaly <islamlang@gmail.com>
2022-11-18 23:18:06 +0200

    Finished tests.py

---
+++ final_project/machinetranslation/tests.py
@@ -0,0 +1,41 @@
+'''
+Unit testing translator.py
+'''
+import unittest
+from translator import englishToFrench, frenchToEnglish
+
+class TestEnglishtoFrench(unittest.TestCase):
+    '''
+    Testing translation from English to French
+    '''
+    def test1(self):
+        '''
+        Testing for null input
+        '''
+        self.assertIsNone(englishToFrench(None))
+
+    def test2(self):
+        '''
+        Testing correctness of input translation for English to French
+        '''
+        self.assertEqual(englishToFrench(["Hello"]), ["Bonjour"])
+
+
+class TestFrenchtoEnglish(unittest.TestCase):
+    '''
+    Testing translation from English to French
+    '''
+
+    def test1(self):
+        '''
+        Testing for null input
+        '''
+        self.assertIsNone(frenchToEnglish(None))
+
+    def test2(self):
+        '''
+        Testing correctness of input translation for French to English
+        '''
+        self.assertEqual(frenchToEnglish(["Bonjour"]), ["Hello"])
+
+unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,3 +1,8 @@
+'''
+Module for using IBM ai translation
+api for translating english to french
+and from french to english
+'''
 import json
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
@@ -13,12 +18,22 @@ translator_instance = LanguageTranslatorV3(version='2018-05-01', authenticator=a
 translator_instance.set_service_url(url)
 
 def englishToFrench(englishText):
-    frenchText = translator_instance.translate(text=englishText, model_id="en-fr").get_result()
-    return [line['translation'] for line in frenchText["translations"][:]]
+    '''
+    function for translating english to french
+    '''
+    if englishText is not None:
+        french_text = translator_instance.translate(text=englishText, model_id="en-fr").get_result()
+        return [line['translation'] for line in french_text["translations"][:]]
+    return None
 
-def FrenchToEnglish(frenchText):
-    englishText = translator_instance.translate(text=frenchText, model_id="fr-en").get_result()
-    return [line['translation'] for line in englishText["translations"][:]]
+def frenchToEnglish(frenchText):
+    '''
+    function for translating french to english
+    '''
+    if frenchText is not None:
+        english_text = translator_instance.translate(text=frenchText, model_id="fr-en").get_result()
+        return [line['translation'] for line in english_text["translations"][:]]
+    return None
 
 if __name__ == "__main__":
     frenchText = translator_instance.translate(text="Hello", model_id="en-fr").get_result()
